a20ece703b9ecbdf49665181db68a6299f585954
Sustain Account <sustain@cs.colostate.edu>
2022-09-11 19:18:03 -0600

    make const vars

--- src/library/metadata.js
+++ src/library/metadata.js
@@ -1,32 +1,95 @@
+const waterSiteIdName = "MonitoringLocationIdentifier";
+const waterSiteCollection = "water_quality_sites";
+const aqiSiteIdName = "monitor_id";
+const aqiSiteCollection = "epa_aqi_monitors";
+
 export const sparsityMetadata = [
+    
+    // Water Quality
     {
         "collection": "water_quality_bodies_of_water",
         "label": "Water Quality: Bodies Of Water",
-        "siteIdName": "MonitoringLocationIdentifier",
-        "siteCollection": "water_quality_sites"
+        "siteIdName": waterSiteIdName,
+        "siteCollection": waterSiteCollection,
+        "additionalSiteInfo": []
     },
     {
         "collection": "water_quality_pipes",
         "label": "Water Quality: Pipes",
-        "siteIdName": "MonitoringLocationIdentifier",
-        "siteCollection": "water_quality_sites"
+        "siteIdName": waterSiteIdName,
+        "siteCollection": waterSiteCollection,
+        "additionalSiteInfo": []
     },
     {
         "collection": "water_quality_streams",
         "label": "Water Quality: Rivers and Streams",
-        "siteIdName": "MonitoringLocationIdentifier",
-        "siteCollection": "water_quality_sites"
+        "siteIdName": waterSiteIdName,
+        "siteCollection": waterSiteCollection,
+        "additionalSiteInfo": []
     },
     {
         "collection": "water_quality_point_data",
         "label": "Water Quality: Point Data",
-        "siteIdName": "MonitoringLocationIdentifier",
-        "siteCollection": "water_quality_sites"
+        "siteIdName": waterSiteIdName,
+        "siteCollection": waterSiteCollection,
+        "additionalSiteInfo": []
     },
+
+    // AQI
     {
         "collection": "epa_aqi_ozone",
         "label": "AQI Ozone",
-        "siteIdName": "monitor_id",
-        "siteCollection": "epa_aqi_monitors"
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_carbon_monoxide",
+        "label": "AQI Carbon Monoxide",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_lead_pm10",
+        "label": "AQI Lead PM10",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_lead_tsp",
+        "label": "AQI Lead TSP",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_nitrogen",
+        "label": "AQI Nitrogen",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_pm10_total",
+        "label": "AQI PM10 Total",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_pm25",
+        "label": "AQI PM25",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
+    },
+    {
+        "collection": "epa_aqi_sulfur_dioxide",
+        "label": "AQI Sulfur Dioxide",
+        "siteIdName": aqiSiteIdName,
+        "siteCollection": aqiSiteCollection,
+        "additionalSiteInfo": []
     }
 ]
