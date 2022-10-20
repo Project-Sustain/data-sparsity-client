30d0d80e8588b8d033f54ec685c5aa98add77a12
Sustain Account <sustain@cs.colostate.edu>
2022-10-20 11:47:56 -0600

    add shapefile wrangling

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -4,3 +4,13 @@ export const colors = {
     tertiary: '#f72585',
     highlight: '#f5f502'
 }
+
+// export const colors = {
+//     primary: '#1d3557',
+//     secondary: '#e63946',
+//     tertiary: '#a8dadc',
+//     highlight: '#f5f502'
+//     // 1d3557
+//     // e63946
+//     // a8dadc
+// }
