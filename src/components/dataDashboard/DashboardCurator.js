06221cfb9971234a31f4e06c15487f171cbba4d1
Sustain Account <sustain@cs.colostate.edu>
2022-11-01 09:42:51 -0600

    refactor

--- src/components/dataDashboard/DashboardCurator.js
+++ src/components/dataDashboard/DashboardCurator.js
@@ -1,6 +1,5 @@
-import { Paper, FormGroup, FormControlLabel, Checkbox, Button, Divider } from "@mui/material";
+import { Paper, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 import { makeStyles } from "@material-ui/core";
-import { useState } from "react";
 
 const useStyles = makeStyles({
     button: {
@@ -17,53 +16,30 @@ const useStyles = makeStyles({
 
 export default function DashboardCurator(props) {
     const classes = useStyles();
-    const [open, setOpen] = useState(true);
-
-    const renderButton = () => {
-        if(open) return null;
-        else {
-            return <Button className={classes.button} onClick={() => setOpen(true)}>Dashboard Menu</Button>
-        }
-    }
-
-    const renderCurator = () => {
-        if(!open) return null;
-        else {
-            return (
-                <Paper elevation={3} className={classes.curator}>
-                    <Button fullWidth onClick={() => setOpen(false)}>Close</Button>
-                    <Divider />
-                    <FormGroup>
-                        {
-                            props.dashboardStatus.map((entry, index) => {
-                                return (
-                                    <FormControlLabel
-                                        key={index}
-                                        control={
-                                            <Checkbox
-                                                checked={entry.check}
-                                                onClick={() => entry.set(!entry.check)}
-                                            />
-                                        }
-                                        label={entry.label}
-                                    />
-                                );
-                            })
-                        }
-                    </FormGroup>
-                </Paper>
-            );
-        }
-    }
 
     if(props.dashboardStatus) {
         return (
-            <>  
-                {renderButton()}
-                {renderCurator()}
-            </>
+            <Paper elevation={3} className={classes.curator}>
+                <FormGroup>
+                    {
+                        props.dashboardStatus.map((entry, index) => {
+                            return (
+                                <FormControlLabel
+                                    key={index}
+                                    control={
+                                        <Checkbox
+                                            checked={entry.check}
+                                            onClick={() => entry.set(!entry.check)}
+                                        />
+                                    }
+                                    label={entry.label}
+                                />
+                            );
+                        })
+                    }
+                </FormGroup>
+            </Paper>
         );
     }
-
     else return null;
 }
