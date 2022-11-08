abb913f8ded0923bb10c85919e4ebefb593b5185
Sustain Account <sustain@cs.colostate.edu>
2022-11-08 14:50:52 -0700

    clean up

--- src/library/binary_search.js
+++ src/library/binary_search.js
@@ -1,26 +1,17 @@
 /**
  * Modified from https://www.geeksforgeeks.org/binary-search-in-javascript/
+ * 
+ * This function is a boiler-plate binary search for a REVERSE ordered list
  */
 
 export const binary_search = (arr, x, start, end) => {
-      
-    // Base Condition
     if (start > end) return end; // Debug this, might be `return start`
-  
-    // Find the middle index
     const mid = Math.floor((start + end)/2);
-  
-    // Compare mid with given key x
     if (arr[mid] === x) return mid;
-         
-    // If element at mid is greater than x,
-    // search in the right half of mid
-    if(arr[mid] > x)
+    if(arr[mid] > x) {
         return binary_search(arr, x, mid+1, end);
-    else
- 
-        // If element at mid is smaller than x,
-        // search in the left half of mid
+    }
+    else {
         return binary_search(arr, x, start, mid-1);
-
+    }
 }
